edcb3721130b0b7ce3a2ca68995857ca0dbcd257
lavender-aa <sodaissoda@gmail.com>
2024-08-03 00:17:42 -0400

    changed playing behavior to match animal well
    before, pushing any direction played a note -- this is not how it works in animal well. now, only a note is played when the player presses the `A` button, and the pitch can be raised by a semitone when the `B` button is held (this is how the animal flute behaves).

--- source/main.lua
+++ source/main.lua
@@ -1,23 +1,25 @@
-import "CoreLibs/object"
-import "CoreLibs/graphics"
+import("CoreLibs/object")
+import("CoreLibs/graphics")
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
-local curr_note = {degree=0, pitch=0}
+local curr_note = { degree = 0, pitch = 0 }
 local synthPlayer = pd.sound.synth.new()
 
-local degree_center = {x=130, y=120}
-local pitch_center = {x=275, y=120}
+local degree_center = { x = 130, y = 120 }
+local pitch_center = { x = 275, y = 120 }
+
+local pressed = pd.buttonIsPressed
 
 function pd.update()
 	gfx.clear()
 
 	-- draw directions to show degree and pitch
 	local width, height = 100, 15
-	gfx.fillRect(degree_center.x - width/2, degree_center.y - height/2, width, height)
+	gfx.fillRect(degree_center.x - width / 2, degree_center.y - height / 2, width, height)
 	width, height = height, width
-	gfx.fillRect(degree_center.x - width/2, degree_center.y - height/2, width, height)
+	gfx.fillRect(degree_center.x - width / 2, degree_center.y - height / 2, width, height)
 	gfx.setLineWidth(3)
 	gfx.drawLine(pitch_center.x - 25, pitch_center.y, pitch_center.x + 25, pitch_center.y)
 
@@ -25,11 +27,15 @@ function pd.update()
 	curr_note = getNoteFromInput()
 
 	-- visually show the note
-	drawDegree(curr_note, degree_center)
-	drawPitch(curr_note, pitch_center)
+	drawDegree(curr_note)
+	drawPitch(curr_note)
 
-	-- play the note
-	playNote(curr_note)
+	-- play the note when a button is pressed
+	if pressed(pd.kButtonA) then
+		playNote(curr_note)
+	else
+		synthPlayer:noteOff()
+	end
 end
 
 -- returns a table containing a degree and a pitch
@@ -38,64 +44,76 @@ function getNoteFromInput()
 	local deg, pit = 0, 0
 
 	-- shorthands
-	local pressed = pd.buttonIsPressed
 	local buttons = {
 		pd.kButtonRight,
 		pd.kButtonDown,
 		pd.kButtonLeft,
-		pd.kButtonUp
+		pd.kButtonUp,
 	}
 
 	-- table to keep track of what directional buttons
 	-- are currently being pressed ('1111' -> 'rdlu')
 	local dir = ""
-	for _, b in ipairs(buttons) do dir = dir .. (pressed(b) and "1" or "0") end
+	for _, b in ipairs(buttons) do
+		dir = dir .. (pressed(b) and "1" or "0")
+	end
 
 	-- figure out what pitch to play
 	pit = 0
-	if pressed(pd.kButtonA) then pit += 1 end
-	if pressed(pd.kButtonB) then pit -= 1 end
+	if pressed(pd.kButtonB) then
+		pit += 1
+	end
 
 	-- check if no buttons are being pressed;
-	-- turn off the tone and return early 
+	-- turn off the tone and return early
 	-- (cannot press two opposite directions at once physically)
-	if synthPlayer == nil then return {digree=0, pitch=pit} end
-	if dir == '0000' then synthPlayer:noteOff() ; return {degree=0, pitch=pit} end
+	if synthPlayer == nil then
+		return { digree = 0, pitch = pit }
+	end
+	if dir == "0000" then
+		synthPlayer:noteOff()
+		return { degree = 0, pitch = pit }
+	end
 
 	-- set the degree number based on what buttons are pressed
 	-- (haha pears get it? like pairs)
-	local pears = {'1000', '1100', '0100', '0110', '0010', '0011', '0001', '1001'}
+	local pears = { "1000", "1100", "0100", "0110", "0010", "0011", "0001", "1001" }
 	for a, b in ipairs(pears) do
-		if dir == b then deg = a end
+		if dir == b then
+			deg = a
+		end
 	end
 
 	-- return the table with these two pieces of information
-	return {degree=deg, pitch=pit}
+	return { degree = deg, pitch = pit }
 end
 
-function drawDegree(note, degree_center)
-
-	-- draw the degree of the note 
+function drawDegree(note)
+	-- draw the degree of the note
 	local x, y = degree_center.x, degree_center.y
 	local radius = 75
-	if note == nil or note.degree == nil or note.degree == 0 then return
-	else 
+	if note == nil or note.degree == nil or note.degree == 0 then
+		return
+	else
 		-- degree=1 starts to the right, then moving by an 8th
 		-- of a revolution around the circle for each next degree
 		-- (see https://www.desmos.com/calculator/9qygmub9fr for an interactive display)
 		local angle = (-2 * math.pi * (note.degree - 1)) / 8
 		x, y = x + radius * math.cos(angle), y - radius * math.sin(angle) -- have to invert y direction because down increases y value
-		
+
 		-- draw the circle
 		gfx.fillCircleAtPoint(x, y, 10)
 	end
 end
 
-function drawPitch(note, pitch_center) 
+function drawPitch(note)
 	-- find the coordinates where the pitch visual should be
 	local x, y = pitch_center.x, pitch_center.y
 
-	if note.pitch == nil then return end
+	if note.pitch == nil then
+		return
+	end
+
 	y = pitch_center.y - 20 * note.pitch
 
 	-- draw the pitch
@@ -104,17 +122,23 @@ end
 
 function playNote(note)
 	local text = ""
-	if note.degree == 0 then return end
+	if note.degree == 0 then
+		return
+	end
 
 	-- add the correct note modifier
-	if note.pitch == 1 then text = text .. "#"
-	elseif note.pitch == -1 then text = text .. "b"
+	if note.pitch == 1 then
+		text = text .. "#"
+	elseif note.pitch == -1 then
+		text = text .. "b"
 	end
 
 	-- add the correct note base and octave
-	local notes = {'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'}
-	text = notes[note.degree]:sub(1, 1) .. text .. notes[note.degree]:sub(2,2)
+	local notes = { "C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5" }
+	text = notes[note.degree]:sub(1, 1) .. text .. notes[note.degree]:sub(2, 2)
 
-	if synthPlayer == nil then return end
+	if synthPlayer == nil then
+		return
+	end
 	synthPlayer:playMIDINote(text)
 end
